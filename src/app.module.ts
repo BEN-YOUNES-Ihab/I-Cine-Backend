4df781689507d208d3629b187a17b8e642d589cb
ihab <ihab.ben.younes@hotmail.com>
2023-10-21 19:47:48 +0200

    merge fix again

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,17 @@ import { UsersModule } from './users/users.module';
 import { MoviesModule } from './movies/movies.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { SessionsModule } from './sessions/sessions.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
-    PrismaModule,
-    AuthModule,
-    UsersModule,
-    MoviesModule,
-    CloudinaryModule,
-    SessionsModule,
-  ],
+    PrismaModule, 
+    AuthModule, 
+    UsersModule, 
+    MoviesModule, 
+    CloudinaryModule, 
+    SessionsModule, 
+    OrdersModule],
   controllers: [],
   providers: [],
 })
