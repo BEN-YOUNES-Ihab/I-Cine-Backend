f1c95ed32c09b67a4aa879e18591d1e0b7c5a045
ihab <ihab.ben.younes@hotmail.com>
2023-10-21 20:56:02 +0200

    merge fix

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,16 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
-  Delete,
-  UploadedFile,
-  UseInterceptors,
-  Query,
-} from '@nestjs/common';
+import { Body, Controller,Get ,Param,ParseIntPipe,Patch,Post, Delete, UploadedFile, UseInterceptors, Query, } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { FilterDto, movieDto } from './dtos/movies.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
@@ -18,37 +6,37 @@ import { updateMovieFormDto } from './dtos/update_movie.dto';
 
 @Controller('movies')
 export class MoviesController {
-  constructor(private movieService: MoviesService) {}
+    constructor(
+        private movieService: MoviesService){}
 
-  @Post('/createMovie')
-  creatMovie(@Body() dto: movieDto) {
-    return this.movieService.createMovie(dto);
-  }
+    @Post('/createMovie')
+    creatMovie(@Body() dto: movieDto){
+        return this.movieService.createMovie(dto);
+    }
 
-  @Patch('/:id/updateMovie')
-  updateMovie(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() dto: updateMovieFormDto,
-  ) {
-    return this.movieService.updateMovie(id, dto);
-  }
+    @Patch('/:id/updateMovie')
+    updateMovie(@Param('id',ParseIntPipe) id:number, @Body() dto: updateMovieFormDto){
+        return this.movieService.updateMovie(id, dto);
+    }
 
-  @Delete('/:id/deleteMovie')
-  deleteMovie(@Param('id', ParseIntPipe) id: number) {
-    return this.movieService.deleteMovie(id);
-  }
+    @Delete('/:id/deleteMovie')
+    deleteMovie(@Param('id',ParseIntPipe) id:number){
+        return this.movieService.deleteMovie(id);
+    }
 
-  @Get('/getMoviesList')
-  getMoviesList(@Query() movieFilterDto: FilterDto) {
-    return this.movieService.getMovies(movieFilterDto);
-  }
+    @Get(':id/getMovie')
+    getMovie(@Param('id', ParseIntPipe) id: number){
+        return this.movieService.getMovie(id);
+    }
 
-  @Post('/:id/upload-image')
-  @UseInterceptors(FileInterceptor('file'))
-  uploadMovieImage(
-    @UploadedFile() file: Express.Multer.File,
-    @Param('id', ParseIntPipe) bookId: number,
-  ) {
-    return this.movieService.uploadMovieImage(bookId, file);
-  }
+    @Get("/getMoviesList")
+    getMoviesList(@Query() movieFilterDto: FilterDto) {
+      return this.movieService.getMovies(movieFilterDto);
+    }
+
+    @Post('/:id/upload-image')
+    @UseInterceptors(FileInterceptor('file'))
+    uploadMovieImage(@UploadedFile() file: Express.Multer.File,@Param('id',ParseIntPipe) bookId:number){
+        return this.movieService.uploadMovieImage(bookId, file);
+    }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -7,134 +7,144 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class MoviesService {
-  constructor(
-    private prismaService: PrismaService,
-    private cloudinaryService: CloudinaryService,
-  ) {}
+    constructor(
+        private prismaService: PrismaService,
+        private cloudinaryService: CloudinaryService
+    ){}
 
-  async createMovie(dto: movieDto) {
-    const movie = await this.prismaService.movie.create({
-      data: {
-        title: dto.title,
-        description: dto.description,
-        releaseDate: new Date(dto.releaseDate),
-        onDisplay: dto.onDisplay,
-        category: dto.category,
-      },
-    });
-    return movie;
-  }
-
-  async getMovie(id: number) {
-    try {
-      const movie = await this.prismaService.movie.findUnique({
-        where: { id: id },
-      });
-      return movie;
-    } catch (e) {
-      if (e instanceof PrismaClientKnownRequestError) {
-        if (e.code === 'P2025') {
-          throw new NotFoundException('Author not found.');
+    async createMovie(dto: movieDto){
+        let date;
+        if(typeof dto.releaseDate === "string"){
+            date= dto.releaseDate;
+        }else{
+            date= dto.releaseDate[0]
         }
-        console.log(e);
-      }
-    }
-  }
-
-  async getMovies(movieFilterDto: FilterDto) {
-    const { keyword, page = '1', size = '10' } = movieFilterDto;
-
-    const skip = (parseInt(page) - 1) * parseInt(size);
-
-    const where = {};
-
-    if (keyword) {
-      where['OR'] = [
-        { title: { contains: keyword } },
-        { category: { contains: keyword } },
-      ];
+        const movie = await this.prismaService.movie.create({
+            data:{ 
+                title:dto.title,
+                description:dto.description,
+                releaseDate:new Date(date),
+                onDisplay:dto.onDisplay,
+                category:dto.category
+            }
+        });
+        return movie
     }
 
-    const movies = await this.prismaService.movie.findMany({
-      where,
-      skip,
-      take: parseInt(size),
-    });
-    const totalElements = await this.prismaService.movie.count({ where }); // Count all matching records.
+    async getMovie(id : number){
+        try{
 
-    const totalPages = Math.ceil(totalElements / parseInt(size));
-
-    return {
-      content: movies,
-      totalElements: totalElements,
-      totalPages: totalPages,
-    };
-  }
-
-  async updateMovie(id: number, dto: updateMovieFormDto) {
-    try {
-      const movie = await this.prismaService.movie.update({
-        where: {
-          id: id,
-        },
-        data: {
-          title: dto.title,
-          description: dto.description,
-          releaseDate: new Date(dto.releaseDate[0]),
-          onDisplay: dto.onDisplay,
-          category: dto.category,
-          updatedAt: new Date(),
-        },
-      });
-      return movie;
-    } catch (e) {
-      if (e instanceof PrismaClientKnownRequestError) {
-        if (e.code === 'P2025') {
-          throw new NotFoundException('Author not found.');
+        const movie = await this.prismaService.movie.findUnique({where :{id:id}})
+        return movie
+        }catch(e){
+            if(e instanceof PrismaClientKnownRequestError){
+                if(e.code ==='P2025'){
+                    throw new NotFoundException('Author not found.');
+                }
+                console.log(e);
+            }      
         }
-        console.log(e);
-      }
-    }
-  }
+    }    
 
-  async deleteMovie(id: number) {
-    try {
-      const movie = await this.prismaService.movie.delete({
-        where: { id: id },
-      });
-      return { message: 'Deleted Successfuly' };
-    } catch (e) {
-      if (e instanceof PrismaClientKnownRequestError) {
-        if (e.code === 'P2025') {
-          throw new NotFoundException('Author not found.');
+    async getMovies(movieFilterDto : FilterDto){
+        const { keyword, page = '1', size = '10' } = movieFilterDto;
+    
+        const skip = (parseInt(page) - 1) * parseInt(size);
+    
+        const where = {};
+
+        if (keyword) {
+          where['OR'] = [
+            { title: { contains: keyword } },
+            { category: { contains: keyword } }
+          ];
         }
-        console.log(e);
-      }
-    }
-  }
+    
+        const movies = await this.prismaService.movie.findMany({
+          where,
+          skip,
+          take: parseInt(size),
+        });
+        const totalElements = await this.prismaService.movie.count({ where }); // Count all matching records.
 
-  async uploadMovieImage(movieId: number, file: Express.Multer.File) {
-    console.log('hihoo');
-    if (!file) {
-      throw new NotFoundException('No file');
+        const totalPages = Math.ceil(totalElements / parseInt(size));
+
+        return{
+            content: movies,
+            totalElements: totalElements,
+            totalPages:totalPages
+        };
     }
-    try {
-      const movie_before = await this.getMovie(movieId);
-      await this.cloudinaryService.deleteFile(
-        movie_before.imageCloudinaryPublicId,
-      );
-    } catch (e) {
-      console.log(e);
+
+    async updateMovie(id:number, dto: updateMovieFormDto){
+        let date;
+        if(typeof dto.releaseDate === "string"){
+            date= dto.releaseDate;
+        }else{
+            date= dto.releaseDate[0]
+        }
+        try{
+            const movie = await this.prismaService.movie.update({
+                where:{
+                    id:id
+                },
+                data: { 
+                    title:dto.title,
+                    description:dto.description,
+                    releaseDate:new Date(date),
+                    onDisplay:dto.onDisplay,
+                    category:dto.category,
+                    updatedAt: new Date()
+                },
+            })
+            return movie
+        }catch(e){
+            if(e instanceof PrismaClientKnownRequestError){
+                if(e.code ==='P2025'){
+                    throw new NotFoundException('Author not found.');
+                }
+                console.log(e);
+            }            
+        }
+
     }
-    const uploadedFile = await this.cloudinaryService.uploadFile(file);
-    console.log(uploadedFile);
-    const movie = await this.prismaService.movie.update({
-      where: { id: movieId },
-      data: {
-        imageUrl: uploadedFile.url,
-        imageCloudinaryPublicId: uploadedFile.public_id,
-      },
-    });
-    return movie;
-  }
+
+    async deleteMovie(id : number){
+        try {
+            const movie = await this.prismaService.movie.delete({
+                where:{id:id}
+            })
+            return {message : 'Deleted Successfuly'}
+        }catch(e){
+            if(e instanceof PrismaClientKnownRequestError){
+                if(e.code ==='P2025'){
+                    throw new NotFoundException('Author not found.');
+                }
+                console.log(e);
+            }            
+        }
+
+    }
+
+    async uploadMovieImage(movieId : number,file: Express.Multer.File){
+        if(!file){
+            throw new NotFoundException('No file');
+        }
+        try{
+            const movie_before = await this.getMovie(movieId);
+            await this.cloudinaryService.deleteFile(movie_before.imageCloudinaryPublicId);
+            
+        }catch(e){
+            console.log(e);
+        }
+        const uploadedFile = await this.cloudinaryService.uploadFile(file);
+        const movie = await this.prismaService.movie.update({
+            where:{id: movieId},
+            data: {
+                imageUrl:uploadedFile.url,
+                imageCloudinaryPublicId: uploadedFile.public_id
+            }
+        })
+        return movie;
+    }
 }
